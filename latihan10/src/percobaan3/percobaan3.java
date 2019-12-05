d9e8e9b8711376756a968171eb4a17bdcb9a83b8
rizalkharyadi <rizal083822286857@gmail.com>
2019-12-05 21:50:43 +0700

    rizal

---
+++ latihan10/src/percobaan3/percobaan3.java
@@ -0,0 +1,17 @@
+package percobaan3;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rizal kharyadi
+ */
+public class percobaan3 {
+    private String name;
+    private String address;
+    private int age;
+    
+}
